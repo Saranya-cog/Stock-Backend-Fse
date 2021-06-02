5d894c707a2d8b0f5865d22470e36dc4a511d609
Saranya-cog <85221331+Saranya-cog@users.noreply.github.com>
2021-06-02 14:45:19 +0530

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockmarket.Models
+{
+    public class User
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        [BsonElement("id")]
+        public string id { get; set; }
+        [BsonElement("CompanyName")]
+        public string CompanyName { get; set; }
+        [BsonElement("CompanyCode")]
+        public string CompanyCode { get; set; }
+        [BsonElement("CompanyCeo")]
+        public string CompanyCeo { get; set; }
+        [BsonElement("Turnover")]
+        public string Turnover { get; set; }
+        [BsonElement("Website")]
+        public string Website { get; set; }
+        [BsonElement("StockExchange")]
+        public List<string> StockExchange { get; set; }
+
+
+    }
+}
---
+++ Models/stock.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockmarket.Models
+{
+    public class stock
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        [BsonElement("CompanyCode")]
+        public string CompanyCode { get; set; }
+        [BsonElement("StockPrice")]
+        public decimal StockPrice { get; set; }
+        [BsonElement("Date")]
+        public DateTime Date { get; set; }
+
+    }
+}
